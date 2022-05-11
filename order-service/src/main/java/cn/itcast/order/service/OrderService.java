b25c6755b58209f1a42a8b543f92be865609387b
hp <431397435@qq.com>
2022-05-11 22:26:34 +0800

    feign优化

---
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -0,0 +1,35 @@
+package cn.itcast.order.service;
+
+import cn.itcast.feign.clients.UserClient;
+import cn.itcast.feign.pojo.User;
+import cn.itcast.order.mapper.OrderMapper;
+import cn.itcast.order.pojo.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class OrderService {
+
+    @Autowired(required = false)
+    private OrderMapper orderMapper;
+
+//    @Autowired
+//    private RestTemplate restTemplate;
+
+    @Autowired
+    private UserClient userClient;
+
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+//        String url = "http://localhost:8081/user/" + order.getUserId();
+//        String url = "http://userservice/user/" + order.getUserId();
+//        User user = restTemplate.getForObject(url, User.class);
+        User user = userClient.findById(order.getUserId());
+        order.setUser(user);
+        // 4.返回
+        return order;
+    }
+}
