4acde151ebda6859ca8715b44d26a2cf1a9172bb
gxw960115 <gxw960115@outlook.com>
2022-05-12 14:12:16 +0800

    "gateway网关"

---
+++ gateway/src/main/java/cn/itcast/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package cn.itcast.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @BelongsProject: cloud-demo
+ * @BelongsPackage: cn.itcast.gateway
+ * @Author: GXW
+ * @CreateTime: 2022-05-12  12:40
+ * @Description: TODO
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
